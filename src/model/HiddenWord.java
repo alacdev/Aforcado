36847a46a56c91aade9aeac81e72bd77725feb33
hugov <hugov@192.168.0.44>
2023-01-27 11:44:08 +0100

    fase 2 acabada

--- src/model/HiddenWord.java
+++ src/model/HiddenWord.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package aforcado;
+package model;
 
 /**
  * Clase que implementa la palabra a adivinar con la que trabajaremos en el juego. 
